41898890feaa7b1d1980bb500af70c4a745c3e4a
Mathieu <ma.scarlatella@gmail.com>
2025-02-08 17:29:33 -0500

    feat: add socials icons

---
+++ src/components/Socials.tsx
@@ -0,0 +1,31 @@
+import { Github, Linkedin } from 'lucide-react';
+import React from 'react';
+import { Button } from './ui/button';
+
+const Socials: React.FC = () => {
+  return (
+    <div className="flex gap-2">
+      <Button
+        size="icon"
+        variant={'link'}
+        onClick={() => window.open('https://github.com/tizano', '_blank')}
+      >
+        <Github color="white"></Github>
+      </Button>
+      <Button
+        size="icon"
+        variant={'link'}
+        onClick={() =>
+          window.open(
+            'https://www.linkedin.com/in/mathieu-scarlatella-0307545b/',
+            '_blank'
+          )
+        }
+      >
+        <Linkedin color="white"></Linkedin>
+      </Button>
+    </div>
+  );
+};
+
+export default Socials;
