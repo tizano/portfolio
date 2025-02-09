a59eb13b2fc6130193154fd077660ca6f76eb59a
Mathieu <ma.scarlatella@gmail.com>
2025-02-09 11:03:42 -0500

    feat: add transition color on socials buttons

--- src/components/Socials.tsx
+++ src/components/Socials.tsx
@@ -8,6 +8,7 @@ const Socials: React.FC = () => {
       <Button
         size="icon"
         variant={'link'}
+        className="hover:bg-zinc-900/50 transition-colors"
         onClick={() => window.open('https://github.com/tizano', '_blank')}
       >
         <Github color="white"></Github>
@@ -15,6 +16,7 @@ const Socials: React.FC = () => {
       <Button
         size="icon"
         variant={'link'}
+        className="hover:bg-zinc-900/50 transition-colors"
         onClick={() =>
           window.open(
             'https://www.linkedin.com/in/mathieu-scarlatella-0307545b/',
